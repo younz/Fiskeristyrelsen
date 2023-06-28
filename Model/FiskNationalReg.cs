599f2cb9ec5b8165c9b0cf1d2583d7ffa5b8a3ad
younas kronborg abachri <younas1996@live.dk>
2023-06-28 10:03:19 +0200

    Færdigt produkt :(

--- Model/FiskNationalReg.cs
+++ Model/FiskNationalReg.cs
@@ -8,6 +8,18 @@ namespace Fiskeristyrelsen.Model
 {
     internal class FiskNationalReg
     {
+        public DateTime Startperiode { get; set; }
+        public DateTime Slutperiode { get; set;}
+        public double AntalFisk { get; set; }
+        public string Art { get; set; }= " ";
 
+        public FiskNationalReg(DateTime startperiode, DateTime slutperiode, double antalFisk, string art)
+        {
+            Startperiode = startperiode;
+            Slutperiode = slutperiode;
+            AntalFisk = antalFisk;
+            Art = art;
+        }
     }
+    
 }
--- Model/SkibJura.cs
+++ Model/SkibJura.cs
@@ -8,13 +8,24 @@ namespace Fiskeristyrelsen.Model
 {
     internal class SkibJura // da dette er til team jura er den kaldt dete for at skabe overblik hvad hver skib skal inholde
     {
-        public string SkibNationalitet { get; private set;}
+        public int SkibId { get; private set; }
+        public string SkibNationalitet { get; private set; } = "Dansk"; /// <summary>
+        /// der blev deafualtet til at skibet flager dansk da dette er den simpleste løsning
+        /// </summary>
         public int Fiskezone { get; private set; }
         public DateTime Afgang { get; private set; }
         public DateTime Ankomst { get; private set; }
         public double Fangstmeande  { get; set; }
         // da disse funktioner for det meste er nogle der kun skal kunne ses på nær mængde der gerne ville kunne ændres har jeg
         // valgt at gøre dem til private 
-
+        public SkibJura(int skibId, string skibNationalitet, int fiskezone, DateTime afgang, DateTime ankomst, double fangstmeande)
+        {
+            SkibId = skibId;
+            SkibNationalitet = skibNationalitet;
+            Fiskezone = fiskezone;
+            Afgang = afgang;
+            Ankomst = ankomst;
+            Fangstmeande = fangstmeande;
+        }
     }
 }
